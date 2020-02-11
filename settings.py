3c2d64d72a5dfc2adeb6c0ff7e874387e0786659
fedix <d.fedor97@gmail.com>
2020-02-11 23:26:42 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        self.ship_speed = 10
+
+        self.bullet_speed = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
